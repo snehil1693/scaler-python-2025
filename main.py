65ee1536c796ae5f1c46610103bcb0e75a6cb0a4
Snehil Shubham <snehil1693@gmail.com>
2025-07-20 20:53:20 +0530

    first file

--- main.py
+++ main.py
@@ -2,3 +2,26 @@
 print("Hello World")
 
 print("Hi")
+
+# # N = int(input())
+# M = int(input())
+#
+# print(N * M)
+
+print('Hello World')
+
+print("Welcome to Scaler")
+
+print("urieuireuirei")
+
+print(10+20)
+
+print("20" + "50")
+
+print("10 + 10")
+
+n1 = 'Ram'
+n2 = 'Shyam'
+
+print(n1 + " says Hi to " + n2)
+
